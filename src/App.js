121603fdb5264b9b083e5ab56a967ff2780bc233
FelicianoAnthonyy <anthony@hbnl.downstate.edu>
2018-04-02 15:48:29 -0400

    ajax get request successfulyl rendered

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
-
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -11,59 +8,64 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: []
+      posts: [],
+      isLoading: false,
+      textInput:[]
     };
+    this.click = this.click.bind(this);
   }
 
   // change response type to text here & make sure test.py returns a string & not json formatted data
-  componentDidMount() {
-    axios.get("http://127.0.0.1:5000/hello/", {
-      responseType:"text"
-    })
+  click() {
+
+    axios.get("http://127.0.0.1:5000/hello/")
       .then(res => {
-        //console.log(posts)
-        this.setState({ posts: res.data });
+        console.log(res.data)
+        this.setState({ posts: res.data,  isLoading: false });
       });
+
   }
+
+  handleChange(value) {
+    this.setState({textInput: value});
+  }
+
 
 
   // prevents text from defaulting to "initial text" after button clicked 
   handleSubmit(event) {
     event.preventDefault();
-    console.log('form submitted')
+
   }
 
   render() {
-    return (
 
-        <div>
-        <h1>{`/r/reactjs}`}</h1>
-        <ul>
-          {this.state.posts}
-        </ul>
+
+    return (
+      // text from test.py will appear here
+      <div className="App">
+        <div id = "left"> Url to be passed as arg to python script
+        <p> {this.state.textInput}</p>
+        </div>
+
+        <div id = "right"> Python data returned from flask server via axios get request  
+        <p > {this.state.posts.map(x => x.some)} </p>
+        </div>
+
+      
+
+        <div className="form_wrapper">
+          <form onSubmit={this.handleSubmit}>
+            <p className = "input-title"> Enter a URL to summarize! </p>
+            <input ref="textBox" type="text" value={this.state.textInput} onChange={ (e) => this.handleChange(e.target.value) } />
+            <div> 
+                <button onClick={this.click} disabled={this.state.isLoading}> click me </button>
+            </div>
+          </form>
+        </div>
+        
       </div>
 
-
-      // <div className="App">
-      // //{this.state.text}
-      //   <div className="form_wrapper">
-      //     <form onSubmit={this.handleSubmit}>
-      //       <input ref="textBox" type="text" />
-      //       //<button onClick={ (e) => { this.clicked(); } } > Click me to summarize! </button>
-      //       <button onClick={this.clicked}> Click me bitch! </button>
-      //     </form>
-      //   </div>
-      // </div>
-
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Welcome to React</h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
     );
   }
 }
--- src/test.py
+++ src/test.py
@@ -1,12 +1,11 @@
 import sys
-from flask_cors import CORS, cross_origin
+
 from flask import Flask, render_template, request, redirect, Response, jsonify
 import random, json
 
 app = Flask(__name__)
-#cors = CORS(app)
-#app.config['CORS_HEADERS'] = 'Content-Type'
 
+# used because were making 'cross domain requests?'
 @app.after_request # blueprint can also be app~~
 def after_request(response):
     header = response.headers
@@ -16,64 +15,10 @@ def after_request(response):
 
 @app.route('/hello/', methods=['GET', 'POST'])
 def hello_world(name=None):
-	di = [{'some': 'text'}]
-	return 'hi from python!'
-	#return json.dumps(di)
-    # buf1 = request.args.get('name')
-    # str = {'key':'Hello World!', 'q':buf1}
-    # #out = {'key':str}
-    # res = json.dumps(str)
-    # return res
+	di = [{'some': 'hi from python!', 'more': 'hi from python1!'}]
+	return json.dumps(di)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# @app.route('/')
-# def output():
-# 	# serve index template
-# 	return render_template('App.js')
-
-# @app.route('/receiver', methods = ['POST'])
-# def worker():
-# 	return 'hi from python!'
-
-# if __name__ == '__main__':
-# 	# run!
-# 	app.run()
-
-
-# tasks = [
-#     {
-#         'id': 1,
-#         'title': u'Buy groceries',
-#         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-#         'done': False
-#     },
-#     {
-#         'id': 2,
-#         'title': u'Learn Python',
-#         'description': u'Need to find a good Python tutorial on the web', 
-#         'done': False
-#     }
-# ]
-
-# @app.route('/todo/api/v1.0/tasks', methods=['POST'])
-# def create_task():
-#     if not request.json or not 'title' in request.json:
-#         abort(400)
-#     task = {
-#         'id': tasks[-1]['id'] + 1,
-#         'title': request.json['title'],
-#         'description': request.json.get('description', ""),
-#         'done': False
-#     }
-#     tasks.append(task)
-#     return jsonify({'task': task}), 201
-
-
-# @app.route('/todo/api/v1.0/tasks', methods=['GET'])
-# def get_tasks():
-#     return jsonify({'tasks': tasks})
-
-
 
